abbae3a5c57e3683eb92e507ca9c98a78172c033
Porfis87 <lrc.porfirio.clavellina@gmail.com>
2020-07-21 19:51:07 -0500

    clase cero agregar

---
+++ clase_reparto/Repaso.js
@@ -0,0 +1,14 @@
+var name = "porfirio"
+//name = "felipe"
+//console.info(name)
+console.info(name)
+
+let last_name = "clavellina"
+console.info(last_name)
+
+//last_name = 23
+//onmouseleave.info(last_name)
+
+
+const age = 25
+
